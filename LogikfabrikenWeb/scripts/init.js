fad96d4fd8f16f04d97b5d38d2be5970382d8394
ericlindersson <eric.lindersson@elicit.se>
2016-05-16 18:35:59 +0200

    Initial commit

---
+++ LogikfabrikenWeb/scripts/init.js
@@ -0,0 +1,9 @@
+﻿(function ($) {
+    $(function () {
+
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+        $('.scrollspy').scrollSpy();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
---
+++ LogikfabrikenWeb/scripts/instagram.js
@@ -0,0 +1,47 @@
+﻿(function ($) {
+
+    var instagramUrl = "https://www.instagram.com/logikfabriken/media/?callback=setInstagramImage";
+
+    function setInstagramImage(media) {
+        var item = media.items[0];
+
+        var caption = item.caption.text;
+        var username = item.user.full_name;
+        var userpicture = item.user.profile_picture;
+        var createdtime = item.created_time;
+        var image = item.images.standard_resolution.url;
+
+        var container = $("#instagram-container");
+        var imageElement = $(".instagram-image", container);
+        var userImageElement = $(".instagram-user", container);
+        var userTextElement = userImageElement.next("span");
+
+
+        imageElement.attr("src", image);
+        userTextElement.text(username);
+        userImageElement.attr("src", userpicture);
+
+    }
+
+
+    $.ajax({
+        url: instagramUrl,
+        type: 'GET',
+        crossDomain: true,
+        success: function(media) {
+            setInstagramImage(media);
+        },
+        error: function (error) {
+            var err = error;
+
+        }
+    });
+
+
+
+
+
+
+
+
+})(jQuery)
